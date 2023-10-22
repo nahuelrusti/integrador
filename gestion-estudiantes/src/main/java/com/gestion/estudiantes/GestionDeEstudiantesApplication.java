a6c96c4331f839cad894f7a69e52a0a62a4b672c
nahuelrusti <nahuellopezroom@gmail.com>
2023-10-21 22:23:34 -0300

    Creación de la gestión de estudiantes

---
+++ gestion-estudiantes/src/main/java/com/gestion/estudiantes/GestionDeEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEstudiantesApplication.class, args);
+	}
+
+}
---
+++ gestion-estudiantes/src/test/java/com/gestion/estudiantes/GestionDeEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
